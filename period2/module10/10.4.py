1379a78b8f9a4cd569727acdcb3081ff7cbfc885
NhiDinh <thaonhi6991@gmail.com>
2023-10-20 14:36:13 +0300

    update

--- period2/module10/10.4.py
+++ period2/module10/10.4.py
@@ -1,29 +1,33 @@
 import random as rd
-class Car:
-    current_speed = 0
-    travelled_distance = 0
 
+
+class Car:
 
     def __init__(self, registration_number, maximum_speed):
         self.registration_number = registration_number
         self.maximum_speed = maximum_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
 
     def acceleration(self, changed_speed):
         sum_speed = self.current_speed + changed_speed
-        if sum_speed >0 and sum_speed <= self.maximum_speed:
+        if 0 < sum_speed <= self.maximum_speed:
             self.current_speed = sum_speed
         elif sum_speed > self.maximum_speed:
             self.current_speed = self.maximum_speed
         else:
             self.current_speed = 0
+
     def drive(self, number_hours):
-        self.travelled_distance += self.current_speed*number_hours
+        self.travelled_distance += self.current_speed * number_hours
+
 
 class Race:
     def __init__(self, name, kilometers, car_list):
         self.name = name
         self.kilometers = kilometers
         self.car_list = car_list
+
     def hour_passes(self):
         for car in self.car_list:
             changed_speed = rd.randint(-10, 15)
@@ -41,20 +45,21 @@ class Race:
                 win = True
         return win
 
+
 list_car = []
 numbers_hour = 0
-for num in range(1,11):
+for num in range(1, 11):
     registration_number = "ABC-" + str(num)
-    car_number = Car(registration_number, (rd.randint(100,200)))
+    car_number = Car(registration_number, (rd.randint(100, 200)))
     list_car.append(car_number)
 
-race1 = Race("Grand Demolition Derby",8000, list_car)
+race1 = Race("Grand Demolition Derby", 8000, list_car)
 
 while True:
     numbers_hour += 1
     race1.hour_passes()
     if numbers_hour % 10 == 0:
-        print(f"After {numbers_hour} minutes:")
+        print(f"After {numbers_hour} hours:")
         race1.print_status()
 
     if race1.race_finished():
